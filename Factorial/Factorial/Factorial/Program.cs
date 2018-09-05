5a4b9d21f15c5844686a90b90f69b5a599819f89
parashar123 <parasharsuraj123@gmail.com>
2018-09-05 19:19:36 +0530

    Add files via upload

---
+++ Factorial/Factorial/Factorial/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter the number for which you want the factorial to be calculated");
+            int FacNum = Convert.ToInt32(Console.ReadLine());
+            long FactorialUsingIteration= IterativeWay(FacNum);
+            long FactorialUsingRecursion= RecurciveWay(FacNum);
+
+            Console.WriteLine("The factorial of " + FacNum + " using Iteration method is {0} \n \n ", FactorialUsingIteration);
+            Console.WriteLine("The factorial of " + FacNum + " using Recursion method is {0} \n \n ", FactorialUsingRecursion);
+            Console.ReadLine();
+        }
+
+        public static long IterativeWay(int myFactorial)
+        {
+          
+
+            int factorial = myFactorial;
+
+            for (int i = factorial - 1; i >= 1; i--)
+            {
+                factorial = factorial * i;
+            }
+            return factorial;
+           
+        }
+
+        public static long RecurciveWay(int myFactorial)
+        {
+            if (myFactorial == 0)
+            {
+                return 1;
+            }
+            else
+            {
+
+                return myFactorial * RecurciveWay(myFactorial - 1);    
+            }
+        }    
+    
+            
+           
+    }
+}
